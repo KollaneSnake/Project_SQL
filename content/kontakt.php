9b17a752b503327956805a250adeaec0ebb9d49e
KollaneSnake <kollanemadu@gmail.com>
2015-12-01 19:30:11 +0200

    Commit

---
+++ content/kontakt.php
@@ -0,0 +1,17 @@
+<div id="column2">
+ 	<h1>Контакт </h1>  
+<div align=left style="width:320px; float:left;">	
+   	<p class="welcome_n">
+    <b>Ida-Virumaa Kutsehariduskeskus</b><br>
+    Aadress: Kutse 13, 41533 Jõhvi, Ida-Virumaa<br>
+Telefon: 332 0381<br>
+E-post: info@ivkhk.ee<br>
+
+ 	</p>
+
+	</div>
+	<div  style="width:550px; padding: 0px 0 0  10px; float:left;">
+	
+	<iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=et&amp;geocode=&amp;q=Kutse+13,+J%C3%B5hvi,+J%C3%B5hvi+vald,+Eesti+Vabariik&amp;aq=0&amp;oq=kutse+13&amp;sll=37.0625,-95.677068&amp;sspn=52.240038,114.169922&amp;ie=UTF8&amp;hq=&amp;hnear=Kutse+13,+J%C3%B5hvi,+41533,+Eesti+Vabariik&amp;t=m&amp;ll=59.367231,27.403851&amp;spn=0.030614,0.145569&amp;z=13&amp;iwloc=A&amp;output=embed"></iframe>
+	</div>
+</div>
